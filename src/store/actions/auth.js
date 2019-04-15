aafa2dbe88338fb2802757f6e1c4ff1479796a27
Rasikh Mukaram <rasikh@192-168-1-156.tpgi.com.au>
2019-04-15 22:02:51 +1000

    fixed auto logout issue

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,9 @@ export const authFail = (error) => {
 }
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -48,7 +51,12 @@ export const auth = (email, password, isSignUp) => {
             url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAYH16BG-1Tqbe7TQfHlyV71Z5uv0TLl0k';
         }
         axios.post(url,authData).
-        then(response => {console.log(response); 
+        then(response => {
+            console.log(response);
+            const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000)
+            localStorage.setItem('token', response.data.idToken);
+            localStorage.setItem('expirationDate', expirationDate); 
+            localStorage.setItem('userId', response.data.localId)
             dispatch(authSuccess(response.data.idToken,response.data.localId)); 
             dispatch(checkAuthTimeout(response.data.expiresIn))}).
             catch(error => {console.log(error); dispatch(authFail(error.response.data.error))});
@@ -60,4 +68,23 @@ export const setAuthRedirectPath = (path) => {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
     }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if(!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if (expirationDate <= new Date()) {
+                dispatch(logout());
+            }
+            else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000))
+            }
+        }
+    };
 }
